65b8192734c2ec72d69521ad7ce1294b24504a09
ken frederick <ken.frederick@gmx.de>
2024-01-02 16:05:08 -0500

    Added Nunjucks templating

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from 'express';
 import fs from 'fs'
 import http from 'http'
 import formidable from 'formidable';
+import * as bodyParser from 'body-parser';
+import * as nunjucks from 'nunjucks';
+import * as path from 'path';
 
 import makeMIDI from './midi';
 import makeHTML from './html';
@@ -11,10 +14,26 @@ import { LsdsngObj } from './lsdsng';
 const port: string | number = process.env.PORT || 3000;
 
 const app = express();
+
+const nunjucksEnv: nunjucks.Environment =
+  nunjucks.configure(path.join(__dirname, './views'), {
+    autoescape: true,
+    express: app,
+  });
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.set('view engine', 'html');
+app.set('engine', nunjucksEnv);
 
 app.get('/', (_req: Request, res: Response) => {
-  res.sendFile(__dirname + '/views/index.html');
+  // res.sendFile(__dirname + '/views/index.html');
+  res.render('./index.njk', {
+    title: 'foo',
+  })
 });
 
 app.post('/', (req: Request, res: Response) => {
